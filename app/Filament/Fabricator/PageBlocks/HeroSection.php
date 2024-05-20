a07e40dc3be4fbfda57f633e23c172d352837b86
Marcos Coelho <mviniciusca@gmail.com>
2024-05-20 16:34:41 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/HeroSection.php
+++ app/Filament/Fabricator/PageBlocks/HeroSection.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Fabricator\PageBlocks;
 
-use Filament\Forms\Components\Builder\Block;
-use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Builder\Block;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class HeroSection extends PageBlock
@@ -13,41 +15,52 @@ class HeroSection extends PageBlock
     {
         return Block::make('hero-section')
             ->schema([
-                TextInput::make('title')
-                    ->placeholder('Title goes here')
-                    ->label('Title of Hero Section')
-                    ->required()
-                    ->maxLength(50),
-                TextInput::make('subtitle')
-                    ->required()
-                    ->placeholder('Subtitle')
-                    ->label('Subtitle of Hero Section')
-                    ->maxLength(140),
-                FileUpload::make('HeroSectionImage')
-                    ->required()
-                    ->directory('hero-section')
-                    ->image()
-                    ->imageEditor(),
-                TextInput::make('btn_text')
-                    ->required()
-                    ->placeholder('Button Text')
-                    ->label('Button Text')
-                    ->maxLength(50),
-                TextInput::make('btn_url')
-                    ->required()
-                    ->placeholder('Button Link')
-                    ->label('Button Link')
-                    ->maxLength(50),
-                TextInput::make('btn_full_text')
-                    ->required()
-                    ->placeholder('Button Text')
-                    ->label('Button Text')
-                    ->maxLength(50),
-                TextInput::make('btn_full_url')
-                    ->required()
-                    ->placeholder('Button Link')
-                    ->label('Button Link')
-                    ->maxLength(50),
+                Section::make('Hero Section')
+                    ->description('Hero Section Settings')
+                    ->icon('heroicon-o-cube')
+                    ->collapsed()
+                    ->schema([
+                        Group::make()
+                            ->columns(2)
+                            ->schema([
+                                TextInput::make('title')
+                                    ->placeholder('Title goes here')
+                                    ->label('Title of Hero Section')
+                                    ->required()
+                                    ->maxLength(50),
+                                TextInput::make('subtitle')
+                                    ->required()
+                                    ->placeholder('Subtitle')
+                                    ->label('Subtitle of Hero Section')
+                                    ->maxLength(140),
+                                FileUpload::make('HeroSectionImage')
+                                    ->required()
+                                    ->columnSpanFull()
+                                    ->directory('hero-section')
+                                    ->image()
+                                    ->imageEditor(),
+                                TextInput::make('btn_text')
+                                    ->required()
+                                    ->placeholder('Button Text')
+                                    ->label('Button Text')
+                                    ->maxLength(50),
+                                TextInput::make('btn_url')
+                                    ->required()
+                                    ->placeholder('Button Link')
+                                    ->label('Button Link')
+                                    ->maxLength(50),
+                                TextInput::make('btn_full_text')
+                                    ->required()
+                                    ->placeholder('Button Text')
+                                    ->label('Button Text')
+                                    ->maxLength(50),
+                                TextInput::make('btn_full_url')
+                                    ->required()
+                                    ->placeholder('Button Link')
+                                    ->label('Button Link')
+                                    ->maxLength(50),
+                            ])
+                    ])
             ]);
     }
 
