e218bd12b5039e4b322fbb95f38da1a9702c4e51
Marcos Coelho <mviniciusca@gmail.com>
2024-05-29 15:35:13 -0300

    wip

--- database/migrations/2024_05_15_143237_create_settings_table.php
+++ database/migrations/2024_05_15_143237_create_settings_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->default('Main Setting');
             $table->timestamps();
         });
     }
