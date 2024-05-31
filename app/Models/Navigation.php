5e7d84074d359d4fd3ff64bb54323dd2c68f47aa
Marcos Coelho <mviniciusca@gmail.com>
2024-05-30 22:26:45 -0300

    wip

--- app/Models/Navigation.php
+++ app/Models/Navigation.php
@@ -2,11 +2,34 @@
 
 namespace App\Models;
 
+use App\Models\Setting;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Navigation extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    /**
+     * TODO: Doc this
+     * @return array
+     */
+    protected function casts(): array
+    {
+        return [
+            'navigation' => 'array',
+        ];
+    }
+
+    /**
+     * Summary of setting
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function setting(): BelongsTo
+    {
+        return $this->belongsTo(Setting::class);
+    }
+
 }
--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Contact;
 use App\Models\Layout;
+use App\Models\Navigation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -13,13 +14,30 @@ class Setting extends Model
     use HasFactory;
     protected $guarded = [];
 
+    /**
+     * Summary of contact
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function contact(): HasOne
     {
         return $this->hasOne(Contact::class);
     }
 
+    /**
+     * Summary of layout
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
     public function layout(): HasOne
     {
         return $this->hasOne(Layout::class);
+    }
+
+    /**
+     * Summary of navigation
+     * @return HasOne
+     */
+    public function navigation(): HasOne
+    {
+        return $this->hasOne(Navigation::class);
     }
 }
