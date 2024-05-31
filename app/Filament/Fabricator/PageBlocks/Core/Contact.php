5f4e3f173a353c1eef685b591c6bda0f028f14d0
Marcos Coelho <mviniciusca@gmail.com>
2024-05-31 13:24:12 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Core/Contact.php
+++ app/Filament/Fabricator/PageBlocks/Core/Contact.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Fabricator\PageBlocks\Core;
 
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Section;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class Contact extends PageBlock
@@ -10,8 +11,13 @@ class Contact extends PageBlock
     public static function getBlockSchema(): Block
     {
         return Block::make('core.contact')
+            ->icon('heroicon-o-cpu-chip')
+            ->label(__('Contact Section'))
             ->schema([
-                //
+                Section::make(__('Contact Section'))
+                    ->description(__('Core Feature for a Contact Section. This can be edited on Settings > Contact & Locale'))
+                    ->icon('heroicon-o-cpu-chip')
+                    ->collapsed()
             ]);
     }
 
