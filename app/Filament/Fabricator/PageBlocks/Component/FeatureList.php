69b152cd591cb134bd546a2d3beb4ba6de92fcaf
Marcos Coelho <mviniciusca@gmail.com>
2024-06-01 13:01:01 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Component/FeatureList.php
+++ app/Filament/Fabricator/PageBlocks/Component/FeatureList.php
@@ -5,8 +5,10 @@ namespace App\Filament\Fabricator\PageBlocks\Component;
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class FeatureList extends PageBlock
@@ -22,28 +24,61 @@ class FeatureList extends PageBlock
                     ->collapsed()
                     ->icon('heroicon-o-squares-2x2')
                     ->schema([
+                        Section::make(__('Settings'))
+                            ->description(__('Settings and configuration of this card\'s section'))
+                            ->icon('heroicon-o-cog-6-tooth')
+                            ->collapsible()
+                            ->columns(3)
+                            ->schema([
+                                Toggle::make('centered')
+                                    ->label(__('Align in the Center'))
+                                    ->helperText(__('Place the card content align in the center'))
+                                    ->inline(false)
+                                    ->default(true),
+                                Toggle::make('icon_filled')
+                                    ->label(__('Icon Background'))
+                                    ->helperText(__('Show icon background'))
+                                    ->inline(false)
+                                    ->default(true),
+                                Select::make('grid_view')
+                                    ->label(__('Cards per Line'))
+                                    ->helperText(__('Show the cards quantity shown per line'))
+                                    ->options([
+                                        'default' => 3,
+                                        '4' => 4,
+                                    ])
+                                    ->default('default')
+                            ]),
                         Repeater::make('cards')
                             ->collapsed()
                             ->cloneable()
                             ->columns(3)
                             ->schema([
                                 TextInput::make('icon')
-                                    ->label('Icon')
-                                    ->prefixIcon('heroicon-o-link')
+                                    ->label(__('Icon Name'))
+                                    ->required()
+                                    ->helperText(__('Set the name of Ionicon\'s icon'))
+                                    ->prefix('ionicon')
                                     ->maxLength(140),
                                 TextInput::make('title')
-                                    ->label('Title')
+                                    ->label(__('Card Title'))
+                                    ->required()
+                                    ->helperText(__('The title of the card'))
                                     ->columnSpan(2)
                                     ->maxLength(100),
-                                Textarea::make('info')
-                                    ->label('Info')
-                                    ->columnSpanFull()
-                                    ->maxLength(200),
                                 TextInput::make('link')
-                                    ->label('Card Link')
+                                    ->label(__('Card Link'))
+                                    ->required()
+                                    ->helperText(__('The link of the card'))
                                     ->columnSpanFull()
                                     ->prefixIcon('heroicon-o-link')
                                     ->maxLength(140),
+                                Textarea::make('info')
+                                    ->label(__('Card Short Description'))
+                                    ->helperText(__('The description of the card'))
+                                    ->columnSpanFull()
+                                    ->rows(3)
+                                    ->maxLength(200),
                             ]),
                     ]),
             ]);
--- resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/feature-list.blade.php
@@ -1,21 +1,24 @@
 @aware(['page'])
-@props(['cards'])
+@props(['cards', 'grid_view', 'centered', 'icon_filled'])
 
 <section>
-    <div class="space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+    <div class="space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0
+        {{ $grid_view === 'default' ? 'lg:grid-cols-3' : 'lg:grid-cols-4' }}">
+
         @foreach ($cards as $card)
         <a href="{{ $card['link'] }}">
-            <div>
-                <div
-                    class="mb-4 flex h-10 w-10 items-center justify-center rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-950">
+            <div class="{{ $centered ? 'text-center' : 'text-left' }}">
+                <div class="{{ $centered ? 'mx-auto' : '' }}
+                mb-4 flex h-10 w-10 items-center justify-center rounded-full lg:h-12 lg:w-12
+                {{ $icon_filled ? 'dark:bg-primary-950 bg-primary-100' : 'bg-none' }}
+                ">
                     <x-ionicon :icon="$card['icon']" />
                 </div>
-                <h3 class="dark:text-white mb-2 text-xl font-bold">{{ $card['title'] }}</h3>
-                <p class="text-gray-500 dark:text-gray-400">
-                    {{ $card['info'] }}
-                </p>
+                <h3 class="mb-2 text-xl font-bold">{{ $card['title'] }}</h3>
+                <p> {{ $card['info'] }} </p>
             </div>
         </a>
         @endforeach
+
     </div>
 </section>
