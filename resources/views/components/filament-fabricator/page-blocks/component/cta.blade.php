e1b6b833e0a6efb748a8661a626e531bf2c293b0
Marcos Coelho <mviniciusca@gmail.com>
2024-06-04 16:56:05 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/cta.blade.php
@@ -10,21 +10,7 @@
                 {!! $title !!}
             </h2>
             <p class="text-gray-500 mb-6 font-light md:text-lg">{!! $subtitle !!}</p>
-            @if($btn_text)
-            @if($btn_url )
-            <a target="{{ $target ? '_blank' : '_self' }}" href="{{ $btn_url }}" @endif
-                class="inline-flex items-center rounded-lg bg-secondary-600 px-5 py-2.5 text-center text-sm font-medium text-primary-50 hover:bg-secondary-500 focus:ring-4 focus:ring-secondary-400 dark:focus:ring-secondary-900">
-                {{ $btn_text }}
-                <svg class="-mr-1 ml-2 h-5 w-5" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                </svg>
-                @if($btn_url)
-            </a>
-            @endif
-            @endif
+            <x-ui.button>{{ $btn_text }}</x-ui.button>
         </div>
     </div>
 </section>
