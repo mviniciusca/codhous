013f9ad73fddef83f73558757d4d13807207333e
Marcos Coelho <mviniciusca@gmail.com>
2024-06-04 17:14:38 -0300

    wip

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,7 +1,7 @@
-@props(['icon' => 'chevron-forward-outline', 'icon_left' => null, 'filled' => false])
+@props(['icon' => 'chevron-forward-outline', 'icon_left' => null, 'filled' => true])
 
 <button
-    class="inline-flex items-center active:opacity-50 rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:ring-4 {{ $filled ? 'bg-secondary-600 hover:bg-secondary-500 text-primary-50 dark:focus:ring-secondary-900 focus:ring-secondary-400' : 'border border-primary-500 dark:border-primary-700 dark:hover:bg-primary-800 dark:hover:border-primary-950 focus:ring-primary-500'}}">
+    class="inline-flex items-center active:opacity-50 rounded-lg px-5 py-2.5 text-center text-sm font-medium focus:ring-4 {{ $filled ? 'bg-secondary-600 hover:bg-secondary-500 text-primary-50 dark:focus:ring-secondary-900 focus:ring-secondary-400' : 'border border-primary-300 dark:border-primary-700 dark:hover:bg-primary-800 dark:hover:border-primary-950 hover:border-primary-200 focus:ring-primary-500 bg-opacity-10 bg-primary-300 dark:text-primary-50 dark:bg-opacity-5'}}">
     <span class="{{ $icon_left ? 'order-1' : 'order-3' }}">
         {{ $slot }}
     </span>
