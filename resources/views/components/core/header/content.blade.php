1189d6f0a88705f3aca8d6a4266811db81c1ace2
Marcos Coelho <mviniciusca@gmail.com>
2024-06-04 22:25:05 -0300

    wip

--- resources/views/components/core/header/content.blade.php
+++ resources/views/components/core/header/content.blade.php
@@ -1,15 +1,17 @@
 @props(['buttons' => null])
 
-@if($buttons)
-@foreach ($buttons['nav_button'] as $button)
+<div class="inline-flex gap-2 py-2">
+    @if($buttons)
+    @foreach ($buttons['nav_button'] as $button)
 
-@if($button['status'])
-<a target="{{$button['target'] ? '_blank' : '_self'}}" href="{{ $button['link'] ? $button['link'] : '#' }}">
-    <x-ui.button :filled="$button['filled']" :iconLeft="$button['iconLeft']" :icon="$button['icon']">
-        {!! $button['title'] !!}
-    </x-ui.button>
-</a>
-@endif
+    @if($button['status'])
+    <a target="{{$button['target'] ? '_blank' : '_self'}}" href="{{ $button['link'] ? $button['link'] : '#' }}">
+        <x-ui.button :filled="$button['filled']" :iconLeft="$button['iconLeft']" :icon="$button['icon']">
+            {!! $button['title'] !!}
+        </x-ui.button>
+    </a>
+    @endif
 
-@endforeach
-@endif
+    @endforeach
+    @endif
+</div>
