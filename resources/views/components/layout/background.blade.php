7fb22f8af99289b72edecfee183dccb4551cf0cb
Marcos Coelho <mviniciusca@gmail.com>
2024-06-07 11:00:03 -0300

    wip

--- resources/views/components/layout/background.blade.php
+++ resources/views/components/layout/background.blade.php
@@ -1,7 +1,7 @@
 @props(['content'])
 
 @if($content)
-@if($status)
+@if($content['status'])
 <div class="absolute -z-10 w-full overflow-hidden" style="height:{{ $content['bg_hight'] . 'px' }};">
     <div class="h-full w-full {{ $content['bg_size'] . ' ' . $content['bg_repeat'] . ' ' . $content['bg_position'] . ' ' . $content['bg_attachment'] }}"
         style="background-image: url('{{ asset('storage/' . ($background ? $background : '')) }}')">
