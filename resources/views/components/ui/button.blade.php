fb2c3adbab9af85aeed28fc22b54966ebc252ae6
Marcos Coelho <mviniciusca@gmail.com>
2024-06-12 17:18:31 -0300

    wip

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -5,7 +5,7 @@
 'filled' => null])
 
 <button type="{{ $type }}"
-    class="inline-flex items-center mx-auto active:opacity-50 rounded-lg px-3 py-2.5 text-center text-sm font-medium focus:ring-4 hover:bg-primary-50 hover:bg-opacity-90  {{ $filled ? 'bg-secondary-600 border border-secondary-900  text-primary-50 dark:focus:ring-secondary-900 dark:hover:bg-secondary-900 bg-opacity-100 hover:bg-opacity-95 hover:bg-secondary-900 focus:ring-secondary-400' : 'border border-primary-300 dark:border-primary-700 dark:hover:bg-primary-800 dark:hover:border-primary-950 hover:border-primary-200 focus:ring-primary-500 bg-opacity-10 bg-primary-300 dark:text-primary-50 dark:bg-opacity-5 dark:bg-primary-50'}}">
+    class="inline-flex items-center mx-auto active:opacity-50 rounded-lg px-3 py-2.5 text-center text-sm font-medium focus:ring-4 hover:bg-primary-50 hover:bg-opacity-90 {{ $filled ? 'bg-secondary-600 border border-secondary-900  text-primary-50 dark:focus:ring-secondary-900 dark:hover:bg-secondary-900 bg-opacity-100 hover:bg-opacity-95 hover:bg-secondary-900 focus:ring-secondary-400' : 'border border-primary-300 dark:border-primary-700 dark:hover:bg-primary-800 dark:hover:border-primary-950 hover:border-primary-200 focus:ring-primary-500 bg-opacity-10 bg-primary-300 dark:text-primary-50 dark:bg-opacity-5 dark:bg-primary-50'}}">
 
     @if($icon && $iconLeft && $icon !== 'none')
     <x-ionicon :$icon />
