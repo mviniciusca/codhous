61f262c0ca393be1dd472ac0a656c2ccd32930db
Marcos Coelho <mviniciusca@gmail.com>
2024-06-14 14:04:59 -0300

    wip

---
+++ resources/views/livewire/newsletter.blade.php
@@ -0,0 +1,31 @@
+<section class="bg-white dark:bg-gray-900">
+    <form action="#">
+        <div class="mx-auto mb-3 max-w-screen-sm items-center space-y-4 sm:flex sm:space-y-0">
+            <div class="relative w-full">
+                <label for="email" class="mb-2 hidden text-sm font-medium text-gray-900 dark:text-gray-300">Email
+                    address</label>
+                <div class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg class="h-5 w-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z">
+                        </path>
+                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                    </svg>
+                </div>
+                <input
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-3 pl-10 text-sm text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-primary-500 dark:focus:ring-primary-500 sm:rounded-none sm:rounded-l-lg"
+                    placeholder="Enter your email" type="email" id="email" required="">
+            </div>
+            <div>
+                <button type="submit"
+                    class="w-full cursor-pointer rounded-lg border border-primary-600 bg-primary-700 px-5 py-3 text-center text-sm font-medium text-white hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:rounded-none sm:rounded-r-lg">Subscribe</button>
+            </div>
+        </div>
+        <div class="newsletter-form-footer mx-auto max-w-screen-sm text-left text-sm text-gray-500 dark:text-gray-300">
+            We care about the protection of your data. <a href="#"
+                class="font-medium text-primary-600 hover:underline dark:text-primary-500">Read our Privacy
+                Policy</a>.</div>
+    </form>
+    </div>
+    </div>
+</section>
