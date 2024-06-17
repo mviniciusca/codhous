65071fbf363e159a1e073f51942db5985c83458a
Marcos Coelho <mviniciusca@gmail.com>
2024-06-17 14:59:35 -0300

    wip

---
+++ app/Filament/Resources/NewsletterResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Newsletter;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\ActionGroup;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\NewsletterResource\Pages;
+use App\Filament\Resources\NewsletterResource\RelationManagers;
+
+class NewsletterResource extends Resource
+{
+    protected static ?string $model = Newsletter::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('email'),
+                IconColumn::make('active')->boolean()->alignCenter()
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\DeleteAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListNewsletters::route('/'),
+            'create' => Pages\CreateNewsletter::route('/create'),
+            'edit' => Pages\EditNewsletter::route('/{record}/edit'),
+        ];
+    }
+}
