b0f0ae20e1ea5a880293d247c21fb4530f74b77f
Marcos Coelho <mviniciusca@gmail.com>
2024-06-17 12:29:26 -0300

    wip

--- resources/views/components/layout/footer.blade.php
+++ resources/views/components/layout/footer.blade.php
@@ -3,12 +3,7 @@
         <footer>
             <div class="mx-auto w-full max-w-screen-xl p-4 md:py-8">
                 <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/"
-                        class="mb-4 flex items-center space-x-3 sm:mb-0 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                        <span
-                            class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">Flowbite</span>
-                    </a>
+                    <x-core.header.logo />
                     <ul class="mb-6 flex flex-wrap items-center text-sm font-medium text-gray-500 sm:mb-0">
                         <li>
                             <a href="#" class="me-4 hover:underline md:me-6">About</a>
@@ -25,8 +20,13 @@
                     </ul>
                 </div>
                 <hr class="my-6 border-gray-200 dark:border-primary-800 sm:mx-auto lg:my-8" />
-                <span class="block text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/"
-                        class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+                <span class="block text-sm text-gray-500 sm:text-center">
+                    © {{ date('Y') }}
+                    <a href="/" class="hover:underline">
+                        {{ ENV('APP_NAME') }}
+                    </a> -
+                    {{ __('All Rights Reserved.') }}
+                </span>
             </div>
         </footer>
     </x-layout.content>
