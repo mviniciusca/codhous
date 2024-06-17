1ce1fcc7988100cd7e9cf2696b1338cf8dd4a2b0
Marcos Coelho <mviniciusca@gmail.com>
2024-06-17 12:25:45 -0300

    wip

--- resources/views/components/core/header.blade.php
+++ resources/views/components/core/header.blade.php
@@ -1,18 +1,16 @@
-<header class="fixed z-10 mx-auto w-full bg-primary-50 bg-opacity-70 bg-blend-multiply shadow-2xl dark:bg-primary-950">
-    <div class="container mx-auto flex flex-col flex-wrap items-center p-3 md:flex-row">
-
-        {{-- Application Logo --}}
-        <a class="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
-            <x-core.header.logo />
-        </a>
-
-        {{-- Menu Navigation //  --}}
-        <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
-            <x-core.header.navigation />
-        </nav>
-
-        {{-- Header Side Content // Button --}}
-        <x-core.header.content />
-
-    </div>
+<header class="fixed z-10 mx-auto w-full bg-primary-100 dark:bg-primary-950">
+    <x-layout.content>
+        <div class="mx-auto flex flex-col flex-wrap items-center md:flex-row">
+            {{-- Application Logo --}}
+            <a class="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
+                <x-core.header.logo />
+            </a>
+            {{-- Menu Navigation //  --}}
+            <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto md:mr-auto">
+                <x-core.header.navigation />
+            </nav>
+            {{-- Header Side Content // Button --}}
+            <x-core.header.content />
+        </div>
+    </x-layout.content>
 </header>
