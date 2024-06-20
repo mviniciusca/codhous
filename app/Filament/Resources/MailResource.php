4cad7c4491bec85be63e1456d148a87a4b243cd1
Marcos Coelho <mviniciusca@gmail.com>
2024-06-20 19:18:01 -0300

    wip

--- app/Filament/Resources/MailResource.php
+++ app/Filament/Resources/MailResource.php
@@ -49,7 +49,7 @@ class MailResource extends Resource
                 TextColumn::make('email'),
                 TextColumn::make('subject'),
             ])
-            ->defaultSort('desc')
+            ->defaultSort('created_at', 'desc')
             ->filters([
                 //
             ])
