4a1cb11e9414897c3a4ebac24b6177d835616a03
Marcos Coelho <mviniciusca@gmail.com>
2024-07-01 11:09:52 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/core/budget-tool.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/core/budget-tool.blade.php
@@ -1,6 +1,6 @@
 @aware(['page'])
-<div class="px-4 py-4 md:py-8">
-    <div class="max-w-7xl mx-auto">
-        //
-    </div>
-</div>
+<x-layout.section>
+    <x-layout.content>
+
+    </x-layout.content>
+</x-layout.section>
