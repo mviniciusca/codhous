0f4f30003e5caffca5cb5372988ddd88c637ca9a
Marcos Coelho <mviniciusca@gmail.com>
2024-07-01 18:35:12 -0300

    wip

--- resources/views/livewire/budget.blade.php
+++ resources/views/livewire/budget.blade.php
@@ -2,7 +2,6 @@
 <div>
     <form wire:submit='create'>
         {{ $this->form }}
-        <input name="code" type="hidden" value="{{ $code }}">
         <x-ui.button>
             Enviar
         </x-ui.button>
