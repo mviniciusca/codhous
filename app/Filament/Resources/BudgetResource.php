16a7924aa324bd508116097318b0cc57e209e0f5
Marcos Coelho <mviniciusca@gmail.com>
2024-07-02 18:44:01 -0300

    wip

--- app/Filament/Resources/BudgetResource.php
+++ app/Filament/Resources/BudgetResource.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources;
 
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Toggle;
 use Filament\Tables;
 use App\Models\Budget;
 use Filament\Forms\Form;
@@ -14,6 +17,9 @@ use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\BudgetResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\BudgetResource\RelationManagers;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 
 class BudgetResource extends Resource
 {
@@ -25,7 +31,39 @@ class BudgetResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Group::make()
+                    ->columnSpanFull()
+                    ->schema([
+                        Section::make(__('Budget Overview'))
+                            ->columns(4)
+                            ->description(__('Organize your budget report'))
+                            ->icon('heroicon-o-document')
+                            ->schema([
+                                Toggle::make('is_active')
+                                    ->helperText(__('Enable or disable this budget from the dashboard view'))
+                                    ->label(__('Active'))
+                                    ->inline(),
+                                TextInput::make('code')
+                                    ->label(__('Budget Code'))
+                                    ->helperText(__('Use this code to search'))
+                                    ->readOnly(),
+                                Select::make('status')
+                                    ->helperText(__('Set the budget status'))
+                                    ->options([
+                                        'pending' => __('Pending'),
+                                        'on going' => __('On Going'),
+                                        'done' => __('Done'),
+                                        'ignored' => __('Ignored'),
+                                    ]),
+                                DateTimePicker::make('created_at')
+                                    ->format('d/m/Y H:i')
+                                    ->readOnly()
+                                    ->label(__('Date'))
+                            ]),
+                    ]),
+                Section::make('Budget Calculator')
+                    ->description(__('Here is the calculator for your budget'))
+                    ->icon('heroicon-o-pencil')
             ]);
     }
 
@@ -38,7 +76,7 @@ class BudgetResource extends Resource
                     ->color(fn(string $state): string => match ($state) {
                         'pending' => 'primary',
                         'on going' => 'warning',
-                        'done' => 'info',
+                        'done' => 'success',
                         'ignored' => 'danger'
                     }),
                 TextColumn::make('content.customer_name')
