36409ee801df800b8d2ac68563584fd4a06427ee
Marcos Coelho <mviniciusca@gmail.com>
2024-07-04 17:03:47 -0300

    wip

--- app/Livewire/Budget.php
+++ app/Livewire/Budget.php
@@ -2,23 +2,30 @@
 
 namespace App\Livewire;
 
-use App\Models\Budget as BudgetModel;
-use Filament\Forms\Components\Fieldset;
-use Filament\Notifications\Notification;
-use Illuminate\Support\Str;
+use Closure;
+use Exception;
+use Filament\Forms\Set;
 use Livewire\Component;
 use Filament\Forms\Form;
+use Illuminate\Support\Str;
 use Filament\Forms\Components\Group;
+use Illuminate\Support\Facades\Http;
+use App\Models\Budget as BudgetModel;
+use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\Checkbox;
-use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Forms\Components\Wizard\Step;
 use Filament\Forms\Components\ToggleButtons;
+use Illuminate\Http\Client\RequestException;
+use Filament\Forms\Components\Actions\Action;
 use Filament\Forms\Components\MarkdownEditor;
+use Illuminate\Validation\ValidationException;
 use Filament\Forms\Concerns\InteractsWithForms;
 
 class Budget extends Component implements HasForms
@@ -88,32 +95,47 @@ class Budget extends Component implements HasForms
                                     ->columnSpanFull()
                                     ->columns(2)
                                     ->schema([
-                                        TextInput::make('content.cep')
+                                        TextInput::make('content.postcode')
                                             ->required()
                                             ->minLength(9)
                                             ->mask('99999-999')
                                             ->placeholder('22022-000')
                                             ->maxLength(9)
-                                            ->helperText(__('CEP for your construction'))
+                                            ->helperText(__('Postcode for your construction'))
                                             ->required()
-                                            ->label(__('Construction Address CEP')),
-                                        TextInput::make('content.customer_name')
-                                            ->required()
-                                            ->helperText(__('Your Full Name'))
-                                            ->label(__('Name')),
-                                        TextInput::make('content.customer_phone')
-                                            ->required()
-                                            ->helperText(__(''))
-                                            ->tel()
-                                            ->mask('(99)99999-9999')
-                                            ->placeholder(_('(xx) XXXX-XXXX'))
-                                            ->helperText(__('Your phone with local area'))
-                                            ->label(__('Phone')),
-                                        TextInput::make('content.customer_email')
-                                            ->required()
-                                            ->email()
-                                            ->helperText(__('Enter your contact email'))
-                                            ->label(__('Email')),
+                                            ->label(__('Construction Address Postcode'))
+                                            ->suffixAction(
+                                                fn($state, Set $set, $livewire) =>
+                                                Action::make('search-action')
+                                                    ->icon('heroicon-o-magnifying-glass')
+                                                    ->action(function () use ($state, $livewire, $set) {
+                                                        $set('content.neighborhood', null);
+                                                        $set('content.street', null);
+                                                        $set('content.city', null);
+                                                        $set('content.state', null);
+                                                        $livewire->validateOnly('data.content.postcode');
+                                                        $cepData = Http::get("https://viacep.com.br/ws/{$state}/json/")
+                                                            ->throw()
+                                                            ->json();
+                                                        if (isset($cepData['erro'])) {
+                                                            throw ValidationException::withMessages([
+                                                                'data.content.postcode' => __('CEP not Found'),
+                                                            ]);
+                                                        }
+                                                        $set('content.neighborhood', $cepData['bairro'] ?? null);
+                                                        $set('content.street', $cepData['logradouro'] ?? null);
+                                                        $set('content.city', $cepData['localidade'] ?? null);
+                                                        $set('content.state', $cepData['uf'] ?? null);
+                                                    })
+                                            ),
+                                        TextInput::make('content.number')->helperText(__('Number'))->label(__('Number')),
+                                        TextInput::make('content.street')->required()->disabled()->helperText(__('Street Address'))->label(__('Street')),
+                                        TextInput::make('content.city')->required()->disabled()->helperText(__('City'))->label(__('City')),
+                                        TextInput::make('content.neighborhood')->required()->disabled()->helperText(__('Neighborhood'))->label(__('Neighborhood')),
+                                        TextInput::make('content.state')->required()->disabled()->helperText(__('UF'))->label(__('UF')),
+                                        TextInput::make('content.customer_name')->required()->helperText(__('Your Full Name'))->label(__('Name')),
+                                        TextInput::make('content.customer_phone')->required()->helperText(__(''))->tel()->mask('(99)99999-9999')->placeholder(_('(xx) XXXX-XXXX'))->helperText(__('Your phone with local area'))->label(__('Phone')),
+                                        TextInput::make('content.customer_email')->required()->email()->helperText(__('Enter your contact email'))->label(__('Email')),
                                     ]),
                             ]),
                     ]),
