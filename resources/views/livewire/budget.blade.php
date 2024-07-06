f289e426f17b768b34a976d891d9df8372342c4e
Marcos Coelho <mviniciusca@gmail.com>
2024-07-06 11:11:45 -0300

    wip

--- resources/views/livewire/budget.blade.php
+++ resources/views/livewire/budget.blade.php
@@ -1,9 +1,9 @@
 @props(['code' => null])
 <div class="inline-flex w-full gap-8">
-    <div class="w-1/3 bg-cover rounded-md bg-no-repeat bg-center"
+    <div class="w-full hidden lg:block lg:w-1/3 bg-cover rounded-md bg-no-repeat bg-center"
         style="background-image: url(https://www.guru.com/blog/wp-content/uploads/2023/04/civil-engineer-duties.jpg)">
     </div>
-    <div class="w-2/3">
+    <div class="w-full lg:w-2/3">
         <form wire:submit='create'>
             {{ $this->form }}
             <span class="inline-flex">
