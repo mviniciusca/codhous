f856b140e4b0d94b38248d693d4e750e60e773ae
Marcos Coelho <mviniciusca@gmail.com>
2024-07-05 23:49:54 -0300

    wip

--- resources/views/livewire/budget.blade.php
+++ resources/views/livewire/budget.blade.php
@@ -10,7 +10,7 @@
                 <x-ui.button :icon="'none'">
                     {{ __('Send') }}
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="size-6">
+                        stroke="currentColor" class="size-6 inline-flex">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                     </svg>
             </span>
