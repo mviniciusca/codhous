82d625e1d43d813205e2d1542828af49699e0f2d
Marcos Coelho <mviniciusca@gmail.com>
2024-07-07 14:49:09 -0300

    wip

--- app/Filament/Fabricator/PageBlocks/Core/BudgetTool.php
+++ app/Filament/Fabricator/PageBlocks/Core/BudgetTool.php
@@ -3,6 +3,8 @@
 namespace App\Filament\Fabricator\PageBlocks\Core;
 
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Section;
 use Z3d0X\FilamentFabricator\PageBlocks\PageBlock;
 
 class BudgetTool extends PageBlock
@@ -10,8 +12,16 @@ class BudgetTool extends PageBlock
     public static function getBlockSchema(): Block
     {
         return Block::make('core.budget-tool')
+            ->label(__('Budget Tool'))
+            ->icon('heroicon-o-shopping-bag')
             ->schema([
-                //
+                Section::make(__('Core Feature: Budget Tool'))
+                    ->description(__('Add a Budget Tool in your Application'))
+                    ->icon('heroicon-o-shopping-bag')
+                    ->collapsed()
+                    ->schema([
+                        Hidden::make('id')
+                    ])
             ]);
     }
 
