d7199c301a60c59c83021c4de95e7a53d4492bb2
Marcos Coelho <mviniciusca@gmail.com>
2024-07-08 13:07:59 -0300

    wip

--- resources/views/components/filament-fabricator/page-blocks/component/header-info.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/component/header-info.blade.php
@@ -5,13 +5,12 @@
 
 <x-layout.section :$section_filled>
     <div class="mx-auto {{ $position === 'center' ? 'text-center' : 'text-left'}}
-        {{ $title_font_size === 'large' ? 'text-4xl md:text-5xl lg:text-6xl' :
-        ($title_font_size === 'small' ? 'text-xl md:text-2xl lg:text-3xl' :
-         'text-xl md:text-2xl lg:text-4xl') }}">
+        {{$title_font_size === 'large' ? 'text-5xl md:text-5xl lg:text-6xl' :
+        ($title_font_size === 'small' ? 'text-xl md:text-2xl lg:text-4xl' : 'text-xl md:text-2xl lg:text-4xl')}}">
 
         <span class="{{ $color === 'dark' ? 'text-primary-800' : ($color === 'light' ? 'text-primary-50' : '' ) }}">
             <h1 class="mb-4 {{ $padding ? 'pt-8' : '' }} font-bold
-            {{ $title_font_size === 'large' ? 'leading-none tracking-tighter' : 'leading-normal tracking-normal' }}">
+            {{ $title_font_size === 'small'  ? 'leading-normal tracking-tighter' :  'leading-none tracking-tighter'}}">
                 {!! $title !!}</h1>
             <p class="mb-8 text-lg font-normal lg:text-xl">
                 {!! $subtitle !!}</p>
