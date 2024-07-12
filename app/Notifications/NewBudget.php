7ac915d6c78ff28607681ef3d92cabf5b2895c3a
Marcos Coelho <mviniciusca@gmail.com>
2024-07-12 15:48:54 -0300

    wip

--- app/Notifications/NewBudget.php
+++ app/Notifications/NewBudget.php
@@ -38,11 +38,15 @@ class NewBudget extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject(__('New Budget Offer. ID:' . $this->budget['id']))
+            ->subject(
+                __('New Budget Offer from ') .
+                $this->budget['content']['customer_name'] .
+                ' ID: ' . $this->budget['id']
+            )
             ->greeting(__('Hey!'))
             ->line(__('You\'ve received a new budget offer.'))
             ->line(__('Code: ' . $this->budget['code']))
-            ->line(__('Customer: ' . $this->budget['content']['customer_name']))
+            ->line(__('Customer: ') . $this->budget['content']['customer_name'])
             ->action(__('View Now'), url(env('APP_URL') . '/admin/budgets/' . $this->budget['id'] . '/edit'))
             ->line(__('Check it out in your Dashboard'));
     }
