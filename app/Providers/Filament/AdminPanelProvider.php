24384bdde55631c7102fa6d9b81779df1e2e7b70
Marcos Coelho <mviniciusca@gmail.com>
2024-07-18 19:05:54 -0300

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\MailSent;
 use App\Filament\Resources\MailResource\Pages\SentMail;
 use App\Filament\Resources\SettingResource;
 use App\Filament\Resources\SettingResource\Pages\EditSetting;
