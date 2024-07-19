c70d388766c00df4dc3d1b642cbe31cabd70cc09
Marcos Coelho <mviniciusca@gmail.com>
2024-07-19 16:20:28 -0300

    wip

--- app/Filament/Resources/MailResource/Pages/BinMail.php
+++ app/Filament/Resources/MailResource/Pages/BinMail.php
@@ -4,6 +4,16 @@ namespace App\Filament\Resources\MailResource\Pages;
 
 use App\Models\Mail;
 use Filament\Actions;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use App\Filament\Resources\MailResource;
@@ -22,9 +32,56 @@ class BinMail extends ListRecords
     public function table(Table $table): Table
     {
         return $table
-            ->query(Mail::onlyTrashed())
+            ->query(
+                Mail::onlyTrashed(),
+            )
             ->columns([
+                IconColumn::make('is_sent')
+                    ->wrap()
+                    ->label(__(''))
+                    ->boolean()
+                    ->trueIcon('heroicon-o-paper-airplane')
+                    ->trueColor('secondary')
+                    ->falseIcon('heroicon-o-inbox')
+                    ->falseColor('primary'),
+                ToggleColumn::make('is_favorite')
+                    ->label(__('Important'))
+                    ->inline()
+                    ->alignCenter()
+                    ->onIcon('heroicon-s-star'),
+                TextColumn::make('name')
+                    ->limit(25)
+                    ->label(__('From')),
                 TextColumn::make('email')
+                    ->limit(25)
+                    ->label(__('Email')),
+                TextColumn::make('subject')
+                    ->limit(30)
+                    ->label(ucfirst(__('Subject'))),
+                TextColumn::make('created_at')
+                    ->label(__('Received'))
+                    ->date('d/m/Y H:i')
+            ])
+            ->searchable()
+            ->striped()
+            ->paginated(25)
+            ->defaultSort('created_at', 'desc')
+            ->filters([
+                //
+            ])
+            ->persistFiltersInSession()
+            ->actions([
+                ActionGroup::make([
+                    DeleteAction::make()->label(__('Trash')),
+                    ForceDeleteAction::make(),
+                    RestoreAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
             ]);
     }
 }
