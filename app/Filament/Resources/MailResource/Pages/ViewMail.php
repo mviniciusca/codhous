a9e53e506aa8ab12a6613ef99f5b7db17ce14cf8
Marcos Coelho <mviniciusca@gmail.com>
2024-07-19 20:25:17 -0300

    wip

--- app/Filament/Resources/MailResource/Pages/ViewMail.php
+++ app/Filament/Resources/MailResource/Pages/ViewMail.php
@@ -3,14 +3,19 @@
 namespace App\Filament\Resources\MailResource\Pages;
 
 use App\Filament\Resources\MailResource;
+use DateTime;
 use Filament\Actions;
-use Filament\Infolists\Components\IconEntry;
-use Filament\Infolists\Components\TextEntry;
-use Filament\Infolists\Infolist;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Resources\Pages\EditRecord;
 use Filament\Resources\Pages\ViewRecord;
 use Illuminate\Contracts\Support\Htmlable;
 
-class ViewMail extends ViewRecord
+class ViewMail extends EditRecord
 {
     protected static string $resource = MailResource::class;
 
@@ -20,32 +25,56 @@ class ViewMail extends ViewRecord
             Actions\DeleteAction::make(),
         ];
     }
-
     public function getTitle(): Htmlable|string
     {
         return __('Mail');
     }
-    public function infolist(Infolist $infolist): Infolist
+
+    public function form(\Filament\Forms\Form $form): \Filament\Forms\Form
     {
-        return $infolist
+        return $form
             ->schema([
-                TextEntry::make('name')
-                    ->label(__('Name:')),
-                TextEntry::make('email')
-                    ->label(__('From:')),
-                TextEntry::make('created_at')
-                    ->date('d-m-Y H:i')
-                    ->label(__('Date:')),
-                TextEntry::make('subject')
-                    ->label(__('Subject:'))
-                    ->columnSpanFull(),
-                IconEntry::make('is_favorite')->boolean(),
-                IconEntry::make('is_read')->boolean(),
-                TextEntry::make('message')
-                    ->label(__('Message:'))
-                    ->columnSpanFull()
-            ])
-            ->columns(3)
-        ;
+                Section::make(__('Actions'))
+                    ->icon('heroicon-o-cog-6-tooth')
+                    ->collapsible()
+                    ->columns(6)
+                    ->schema([
+                        Toggle::make('is_favorite')
+                            ->inline(false)
+                            ->label(__('Mark as Important')),
+                        Toggle::make('is_span')
+                            ->inline(false)
+                            ->label(__('Mark as Spam')),
+                        Toggle::make('is_read')
+                            ->inline(false)
+                            ->label(__('Mark as Read')),
+                    ]),
+                Section::make(__('Mail Message'))
+                    ->icon('heroicon-o-envelope')
+                    ->collapsible()
+                    ->columns(5)
+                    ->schema([
+                        TextInput::make('name')
+                            ->label(__('From:'))
+                            ->disabled(),
+                        TextInput::make('email')
+                            ->label(__('Email:'))
+                            ->disabled(),
+                        TextInput::make('subject')
+                            ->label(__('Subject:'))
+                            ->columnSpan(2)
+                            ->disabled(),
+                        DateTimePicker::make('created_at')
+                            ->label(__('Date / Hour: '))
+                            ->format('d/m/y H:i:s')
+                            ->disabled(),
+                        Textarea::make('message')
+                            ->label(__('Message:'))
+                            ->columnSpanFull()
+                            ->rows(5)
+                            ->disabled(),
+                    ])
+            ]);
     }
+
 }
