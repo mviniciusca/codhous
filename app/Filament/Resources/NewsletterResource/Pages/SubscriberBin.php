eb564e7b51c6fee9eb66e37045868a175c17875c
Marcos Coelho <mviniciusca@gmail.com>
2024-07-22 15:13:20 -0300

    wip

--- app/Filament/Resources/NewsletterResource/Pages/SubscriberBin.php
+++ app/Filament/Resources/NewsletterResource/Pages/SubscriberBin.php
@@ -4,11 +4,16 @@ namespace App\Filament\Resources\NewsletterResource\Pages;
 
 use Filament\Actions;
 use App\Models\Newsletter;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+use Filament\Tables\Actions\RestoreBulkAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Resources\Pages\ListRecords;
 use Filament\Tables\Columns\ToggleColumn;
 use App\Filament\Resources\NewsletterResource;
-use Z3d0X\FilamentFabricator\Resources\PageResource\Pages\ViewPage;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class SubscriberBin extends ListRecords
 {
@@ -21,6 +26,7 @@ class SubscriberBin extends ListRecords
     public function table(\Filament\Tables\Table $table): \Filament\Tables\Table
     {
         return $table
+            ->searchable()
             ->query(Newsletter::onlyTrashed())
             ->columns([
                 ToggleColumn::make('is_active')
@@ -29,6 +35,20 @@ class SubscriberBin extends ListRecords
                     ->label(__('Name')),
                 TextColumn::make('email')
                     ->label(__('Email')),
+            ])
+            ->filters([])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ])
+            ]);
+    }
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
             ]);
     }
 }
