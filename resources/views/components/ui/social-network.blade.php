f80365f4ddfd8ddb6fc62cf63947adb9617438ff
Marcos Coelho <mviniciusca@gmail.com>
2024-07-23 18:52:06 -0300

    wip

--- resources/views/components/ui/social-network.blade.php
+++ resources/views/components/ui/social-network.blade.php
@@ -1,4 +1,6 @@
 <div>
+    @if($content->social !== null)
+
     @foreach ($content->social as $item )
     <div class="inline-flex p-1 hover:opacity-70 cursor-pointer">
         <a target="new" href="{{ $item['link'] }}">
@@ -6,4 +8,6 @@
         </a>
     </div>
     @endforeach
+
+    @endif
 </div>
