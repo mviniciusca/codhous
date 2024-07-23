f1f3578959389d1423c8f0356ca7657f336f3a9b
Marcos Coelho <mviniciusca@gmail.com>
2024-07-23 14:55:50 -0300

    wip

--- app/Filament/Resources/BudgetResource/Pages/BudgetBin.php
+++ app/Filament/Resources/BudgetResource/Pages/BudgetBin.php
@@ -2,11 +2,26 @@
 
 namespace App\Filament\Resources\BudgetResource\Pages;
 
-use App\Filament\Resources\BudgetResource;
-use App\Models\Budget;
 use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
+use App\Models\Budget;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ForceDeleteAction;
+use Filament\Tables\Actions\RestoreAction;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Resources\Pages\ListRecords;
+use App\Filament\Resources\BudgetResource;
+use Filament\Tables\Actions\ForceDeleteBulkAction;
+use Filament\Tables\Actions\RestoreBulkAction;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class BudgetBin extends ListRecords
 {
@@ -18,12 +33,79 @@ class BudgetBin extends ListRecords
         return $count !== 0 ? $count : null;
     }
 
-    public function table(\Filament\Tables\Table $table): \Filament\Tables\Table
+    public function table(Table $table): Table
     {
         return $table
             ->query(Budget::onlyTrashed())
             ->columns([
                 TextColumn::make('code')
+                    ->searchable()
+                    ->label(__('Code')),
+                TextColumn::make('status')
+                    ->sortable()
+                    ->badge()
+                    ->color(fn(string $state): string => match ($state) {
+                        'pending' => 'primary',
+                        'on going' => 'warning',
+                        'done' => 'success',
+                        'ignored' => 'danger'
+                    }),
+                TextColumn::make('content.customer_name')
+                    ->sortable()
+                    ->searchable()
+                    ->label(__('Name')),
+                TextColumn::make('content.customer_email')
+                    ->label(__('Email')),
+                TextColumn::make('content.customer_phone')
+                    ->label(__('Phone')),
+                TextColumn::make('created_at')
+                    ->date('d/m/Y H:i')
+                    ->sortable()
+                    ->label(__('Date')),
+                IconColumn::make('is_active')
+                    ->label(__('Active'))
+                    ->alignCenter()
+                    ->boolean()
+            ])
+            ->defaultSort('id', 'desc')
+            ->filters([
+                TernaryFilter::make('is_active')
+                    ->placeholder(__('Default'))
+                    ->label(__('Show Budgets'))
+                    ->trueLabel(__('Active'))
+                    ->falseLabel(__('Inactive')),
+                SelectFilter::make('status')
+                    ->placeholder(__('All Status'))
+                    ->label(__('Status'))
+                    ->options([
+                        'pending' => __('Pending'),
+                        'on going' => __('On Going'),
+                        'done' => __('Done'),
+                        'ignored' => __('Ignored'),
+                    ])
+                    ->searchable(),
+            ])
+            ->actions([
+                ActionGroup::make([
+                    DeleteAction::make(),
+                    ForceDeleteAction::make(),
+                    RestoreAction::make(),
+                ])
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
             ]);
     }
 }
