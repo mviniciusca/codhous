cd7b5821bc05ef554442e53ba3d24bcb5ca11233
Marcos Coelho <mviniciusca@gmail.com>
2024-08-02 15:24:54 -0300

    wip

--- resources/views/components/ui/whatsapp.blade.php
+++ resources/views/components/ui/whatsapp.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if ($data->whatsapp['status'])
-    <a target="" href="">
+    <a target="_new" href="{{ env('WPP_URL') . $data->whatsapp['phone']}}">
         <div
             class="w-16 h-16 bg-green-600 hover:bg-green-500 cursor-pointer fixed z-50  bottom-0 mb-5 mr-5 animate-pulse grid items-center justify-center right-0 text-white text-center rounded-full">
             <ion-icon class="text-4xl p-2" name="logo-whatsapp"></ion-icon>
