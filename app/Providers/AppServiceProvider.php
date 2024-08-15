e2e392bb32f2fb710eb3a806794df0dabf41e0d0
Marcos Coelho <mviniciusca@gmail.com>
2024-08-15 17:41:31 -0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $value = Setting::select(['maintenance_mode'])->first()->maintenance_mode;
+        View::share('maintenance_mode', $value);
     }
 }
--- resources/views/components/maintenance/section.blade.php
+++ resources/views/components/maintenance/section.blade.php
@@ -1,3 +1,4 @@
+@if($maintenance_mode)
 <div class="absolute w-full h-full z-50 bg-primary-50 dark:bg-primary-900 justify-center grid">
     <section class="grid items-center justify-center">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -18,3 +19,4 @@
         </div>
     </section>
 </div>
+@endif
