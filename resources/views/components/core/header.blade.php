82f9e3b8913e8a2577a26cae34f1e66bb6a1786c
Marcos Coelho <mviniciusca@gmail.com>
2024-08-28 13:55:36 -0300

    wip

--- resources/views/components/core/header.blade.php
+++ resources/views/components/core/header.blade.php
@@ -1,11 +1,14 @@
 <header class="fixed z-10 mx-auto w-full bg-primary-100 dark:bg-primary-950">
+    @if($maintenance_mode && $discovery_mode && auth()->hasUser())
+    <x-maintenance.discovery />
+    @endif
     <x-layout.content>
         <div class="mx-auto flex flex-col flex-wrap items-center md:flex-row">
             {{-- Application Logo --}}
             <a class="title-font flex items-center font-medium text-gray-900 md:mb-0">
                 <x-core.header.logo />
             </a>
-            {{-- Menu Navigation //  --}}
+            {{-- Menu Navigation // --}}
             <nav class="flex flex-wrap items-center md:ml-auto md:mr-auto">
                 <x-core.header.navigation />
             </nav>
---
+++ resources/views/components/maintenance/discovery.blade.php
@@ -0,0 +1,16 @@
+<div>
+    <div class="text-xs  bg-primary-200 dark:bg-primary-900 w-full border-b
+         border-b-primary-100 dark:border-b-primary-700 animate-pulse">
+        <x-layout.content>
+            <div class="flex gap-2 items-center">
+                <span>
+                    <x-ionicon />
+                </span>
+                {{
+                __('Discovery Mode is enabled. This application is under
+                maintenance for the visitors and visible for you.')
+                }}
+            </div>
+        </x-layout.content>
+    </div>
+</div>
