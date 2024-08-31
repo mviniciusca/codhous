4c1131761d4e31bc970472f0dbf3306ffd0ba5cb
Marcos Coelho <mviniciusca@gmail.com>
2024-08-31 11:24:25 -0300

    wip

--- resources/views/components/maintenance/discovery.blade.php
+++ resources/views/components/maintenance/discovery.blade.php
@@ -1,11 +1,9 @@
 <div>
-    <div class="text-xs bg-primary-200 dark:bg-primary-900 w-full border-b
-         border-b-primary-300 dark:border-b-primary-700 ">
+    <div class="text-xs bg-primary-50 dark:bg-primary-900 w-full border-b
+         border-b-primary-300 dark:border-b-primary-700 animate-pulse">
         <x-layout.content>
             <div class="flex items-center align-middle">
-                <span class="animate-pulse">
-                    <x-ionicon :icon="'glasses-outline'" :size="'big'" />
-                </span>
+                <x-ionicon :icon="'glasses-outline'" :size="'big'" />
                 {{
                 __('Discovery Mode is enabled. This application is under
                 maintenance for the visitors and visible for you.')
