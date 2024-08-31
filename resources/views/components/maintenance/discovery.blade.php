bb389320b984b353e1f748c09c0d820911225d6d
Marcos Coelho <mviniciusca@gmail.com>
2024-08-31 11:36:53 -0300

    wip

--- resources/views/components/maintenance/discovery.blade.php
+++ resources/views/components/maintenance/discovery.blade.php
@@ -2,7 +2,7 @@
     <div class="text-xs bg-primary-50 dark:bg-primary-900 w-full border-b
          border-b-primary-300 dark:border-b-primary-700 animate-pulse">
         <x-layout.content>
-            <div class="flex items-center align-middle">
+            <div class="flex items-center font-bold align-middle">
                 <x-ionicon :icon="'glasses-outline'" :size="'big'" />
                 {{
                 __('Discovery Mode is enabled. This application is under
