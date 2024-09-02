8af884c95fe88dd6ff1356647eb03f3453fb317c
Marcos Coelho <mviniciusca@gmail.com>
2024-09-02 16:49:37 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditMaintenance.php
+++ app/Filament/Resources/SettingResource/Pages/EditMaintenance.php
@@ -31,36 +31,44 @@ class EditMaintenance extends EditRecord
         return $form
             ->schema([
                 Section::make(__('Security & Management'))
-                    ->description(__('Control the visibility of application'))
+                    ->description(__('Control the visibility of application.'))
                     ->icon('heroicon-o-shield-exclamation')
                     ->collapsible()
                     ->columns(2)
                     ->schema([
                         Toggle::make('maintenance_mode')
-                            ->helperText(__('Active or disable the maintenance mode of the application'))
+                            ->helperText(__('Active or disable the maintenance mode of the application.'))
                             ->label(__('Maintenance Mode'))
                             ->inline(),
                         Toggle::make('discovery_mode')
-                            ->helperText(__('See the application when Maintenance Mode is active'))
+                            ->helperText(__('See the application when Maintenance Mode is active.'))
                             ->label(__('Discovery Mode'))
                             ->inline(),
                     ]),
-                Section::make(__('Maintenance Page Design'))
-                    ->description(__('Configure the maintenance page design layout'))
+                Section::make(__('Maintenance Mode Page Design'))
+                    ->description(__('Configure the maintenance page design layout.'))
                     ->icon('heroicon-o-paint-brush')
                     ->collapsible()
                     ->schema([
+                        Toggle::make('show_social')
+                            ->label(__('Social Network Module'))
+                            ->helperText(__('Show social network links if they available'))
+                            ->inline(true)
+                            ->default(true),
                         TextInput::make('title')
                             ->label(__('Title'))
                             ->required()
-                            ->helperText(__('Define the main tittle of the maintenance mode')),
+                            ->placeholder(__('Maintenance Mode'))
+                            ->helperText(__('Define the main tittle of the maintenance mode page.')),
                         Textarea::make('message')
                             ->label(__('Message'))
                             ->required()
-                            ->helperText(__('Define the main message of the maintenance mode')),
+                            ->placeholder(__('This page is under maintenance mode. Please, come back soon! Thanks.'))
+                            ->helperText(__('Define the main message of the maintenance mode page.')),
                         FileUpload::make('image')
                             ->image()
                             ->columnSpanFull()
+                            ->helperText(__('Upload the image for maintenance page.'))
                             ->directory('maintenance')
                             ->label(__('Image Upload')),
                     ]),
