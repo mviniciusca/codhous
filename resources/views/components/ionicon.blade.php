fe77c7d30731bc30f3cc8f59b01622e1be958cab
Marcos Coelho <mviniciusca@gmail.com>
2024-09-02 18:28:08 -0300

    wip

--- resources/views/components/ionicon.blade.php
+++ resources/views/components/ionicon.blade.php
@@ -1,3 +1,6 @@
 @props(['icon' => null, 'size' => null])
 
-<ion-icon class="px-1 {{ $size == 'big' ? 'text-3xl' : 'text-xl' }}" name="{{ $icon }}"></ion-icon>
+<ion-icon
+    class="hover:opacity-60 cursor-pointer active:opacity-40 transition-all duration-100 px-1 {{ $size == 'big' ? 'text-3xl' : 'text-xl' }}"
+    name="{{ $icon }}">
+</ion-icon>
--- resources/views/components/ui/social-network.blade.php
+++ resources/views/components/ui/social-network.blade.php
@@ -4,7 +4,7 @@
     @if($content->social !== null)
 
     @foreach ($content->social as $item )
-    <div class="inline-flex p-1 hover:opacity-70 cursor-pointer">
+    <div class="inline-flex p-1 cursor-pointer">
         <a target="new" href="{{ $item['link'] }}">
             <x-ionicon :size="$size" :icon="'logo-'.$item['icon']" />
         </a>
