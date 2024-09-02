6fee34e5701adfb8d1228646f435cb6d4b1d6525
Marcos Coelho <mviniciusca@gmail.com>
2024-09-02 20:53:37 -0300

    wip

--- resources/views/components/ionicon.blade.php
+++ resources/views/components/ionicon.blade.php
@@ -1,6 +1,5 @@
 @props(['icon' => null, 'size' => null])
 
-<ion-icon
-    class="hover:opacity-60 cursor-pointer active:opacity-40 transition-all duration-100 px-1 {{ $size == 'big' ? 'text-3xl' : 'text-xl' }}"
-    name="{{ $icon }}">
+<ion-icon class="hover:opacity-60 cursor-pointer active:opacity-40 transition-all duration-100 px-1 
+    {{ $size == 'big' ? 'text-3xl' : 'text-xl' }}" name="{{ $icon }}">
 </ion-icon>
