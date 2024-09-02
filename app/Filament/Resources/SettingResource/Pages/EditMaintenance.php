689635e150eade9b19f9acdaba0cf619d3b9c4ff
Marcos Coelho <mviniciusca@gmail.com>
2024-09-02 12:23:27 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditMaintenance.php
+++ app/Filament/Resources/SettingResource/Pages/EditMaintenance.php
@@ -7,30 +7,41 @@ use Filament\Forms\Form;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Section;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Contracts\Support\Htmlable;
 use App\Filament\Resources\SettingResource;
 
 class EditMaintenance extends EditRecord
 {
     protected static string $resource = SettingResource::class;
+    protected static ?string $navigationIcon = 'heroicon-o-shield-exclamation';
+    public static function getNavigationLabel(): string
+    {
+        return __('Security & Management');
+    }
+    public function getTitle(): string|Htmlable
+    {
+        return __('Security & Management Settings');
+    }
 
     public function form(Form $form): Form
     {
-        return $form->schema([
-            Section::make(__('Security & Management'))
-                ->description(__('Control the visibility of application'))
-                ->icon('heroicon-o-shield-exclamation')
-                ->collapsible()
-                ->columns(2)
-                ->schema([
-                    Toggle::make('maintenance_mode')
-                        ->helperText(__('Active or disable the maintenance mode of the application'))
-                        ->label(__('Maintenance Mode'))
-                        ->inline(),
-                    Toggle::make('discovery_mode')
-                        ->helperText(__('See the application when Maintenance Mode is active'))
-                        ->label(__('Discovery Mode'))
-                        ->inline(),
-                ]),
-        ]);
+        return $form
+            ->schema([
+                Section::make(__('Security & Management'))
+                    ->description(__('Control the visibility of application'))
+                    ->icon('heroicon-o-shield-exclamation')
+                    ->collapsible()
+                    ->columns(2)
+                    ->schema([
+                        Toggle::make('maintenance_mode')
+                            ->helperText(__('Active or disable the maintenance mode of the application'))
+                            ->label(__('Maintenance Mode'))
+                            ->inline(),
+                        Toggle::make('discovery_mode')
+                            ->helperText(__('See the application when Maintenance Mode is active'))
+                            ->label(__('Discovery Mode'))
+                            ->inline(),
+                    ]),
+            ]);
     }
 }
