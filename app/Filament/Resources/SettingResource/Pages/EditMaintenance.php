7c9470397ff0a299cd938e9eb564ae7719a7b973
Marcos Coelho <mviniciusca@gmail.com>
2024-09-02 12:11:42 -0300

    wip

---
+++ app/Filament/Resources/SettingResource/Pages/EditMaintenance.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\SettingResource\Pages;
+
+use App\Filament\Resources\SettingResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMaintenance extends EditRecord
+{
+    protected static string $resource = SettingResource::class;
+}
