4e2a0560f1b43322e8a77e5977687491b3416e83
Marcos Coelho <mviniciusca@gmail.com>
2024-09-05 19:12:05 -0300

    wip

---
+++ app/Filament/Widgets/AppInfoWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class AppInfoWidget extends Widget
+{
+    protected static string $view = 'filament.widgets.app-info-widget';
+}
