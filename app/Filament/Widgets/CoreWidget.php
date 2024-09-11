03f6aaff68e8e39a42685d27fa91da1aaed2da20
Marcos Coelho <mviniciusca@gmail.com>
2024-09-11 19:06:32 -0300

    wip

--- app/Filament/Widgets/CoreWidget.php
+++ app/Filament/Widgets/CoreWidget.php
@@ -9,15 +9,27 @@ use Filament\Widgets\TableWidget as BaseWidget;
 
 class CoreWidget extends BaseWidget
 {
+    // protected int|string|array $columnSpan = 'full';
+    protected static ?int $sort = 0;
     public function table(Table $table): Table
     {
         return $table
+            ->heading(__('Core Features'))
             ->query(Module::query()
                 ->select()
                 ->take(5))
             ->columns([
+                ToggleColumn::make('module.header')
+                    ->label(__('Header')),
+                ToggleColumn::make('module.contact')
+                    ->label(__('Contact')),
+                ToggleColumn::make('module.budget')
+                    ->label(__('Budget Tool')),
+                ToggleColumn::make('module.newsletter')
+                    ->label(__('Newsletter')),
                 ToggleColumn::make('module.footer')
-                    ->label(__('Footer'))
-            ]);
+                    ->label(__('Footer')),
+            ])
+            ->paginated(false);
     }
 }
