6d70eb0f4d75d232ac498516ad274cd518ddaa1f
Marcos Coelho <mviniciusca@gmail.com>
2024-09-11 19:24:45 -0300

    wip

---
+++ app/Filament/Widgets/MailWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Mail;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+
+class MailWidget extends BaseWidget
+{
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                Mail::query()
+                    ->where('is_spam', false)
+                    ->where('is_read', false)
+                    ->where('is_sent', false)
+                    ->limit(5)
+            )
+            ->columns([
+                TextColumn::make('name')
+                    ->label(__('Name'))
+                    ->limit(30),
+                TextColumn::make('subject')
+                    ->label(__('Subject'))
+                    ->limit(30),
+            ])
+            ->striped()
+            ->paginated(false);
+    }
+}
