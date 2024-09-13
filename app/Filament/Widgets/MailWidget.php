2fbe2a40307240923cafd30fe65b2baa659d5d57
Marcos Coelho <mviniciusca@gmail.com>
2024-09-13 17:16:24 -0300

    wip

--- app/Filament/Widgets/MailWidget.php
+++ app/Filament/Widgets/MailWidget.php
@@ -3,19 +3,30 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Mail;
+use Filament\Forms\Components\Textarea;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Table;
-use Filament\Widgets\TableWidget as BaseWidget;
 use Filament\Tables\Actions\Action;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Widgets\TableWidget as BaseWidget;
 
 class MailWidget extends BaseWidget
 {
     protected static ?int $sort = 5;
+    public Mail $mail;
+    protected static ?string $model = Mail::class;
     //protected int|string|array $columnSpan = 'full';
     public function table(Table $table): Table
     {
         return $table
+            ->recordUrl(
+                fn(Mail $record): string => route('filament.admin.resources.mails.view', ['record' => $record]),
+            )
             ->description(__('Quick view in your unread messages here.'))
             ->query(
                 Mail::query()
@@ -31,6 +42,21 @@ class MailWidget extends BaseWidget
                 TextColumn::make('subject')
                     ->label(__('Subject'))
                     ->limit(30),
+            ])
+            ->actions([
+                ActionGroup::make([
+                    ViewAction::make()
+                        ->label(__('Quick View'))
+                        ->form([
+                            TextInput::make('name'),
+                            TextInput::make('email'),
+                            TextInput::make('subject'),
+                            Textarea::make('message'),
+                        ]),
+                    DeleteAction::make()
+                        ->requiresConfirmation(),
+                ])
+                    ->model(Mail::class),
             ])
             ->headerActions([
                 Action::make('edit')
