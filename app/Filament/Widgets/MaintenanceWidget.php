945ad2ce90af46ca0ecb674d2ed5d67c4669dfcf
Marcos Coelho <mviniciusca@gmail.com>
2024-09-12 21:57:58 -0300

    wip

--- app/Filament/Widgets/MaintenanceWidget.php
+++ app/Filament/Widgets/MaintenanceWidget.php
@@ -4,7 +4,9 @@ namespace App\Filament\Widgets;
 
 use Filament\Tables;
 use App\Models\Setting;
+use Filament\Forms\Get;
 use Filament\Tables\Table;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Widgets\TableWidget as BaseWidget;
 
 class MaintenanceWidget extends BaseWidget
@@ -16,12 +18,16 @@ class MaintenanceWidget extends BaseWidget
             ->query(
                 Setting::query()
                     ->select()
-                    ->take(5)
             )
+            ->heading(__('Quick Access'))
+            ->description(__('Manage your app visibility here.'))
             ->paginated(false)
             ->striped()
             ->columns([
-                // ...
+                ToggleColumn::make('maintenance_mode')
+                    ->label(__('Maintenance Mode')),
+                ToggleColumn::make('discovery_mode')
+                    ->label(__('Discovery Mode')),
             ]);
     }
 }
