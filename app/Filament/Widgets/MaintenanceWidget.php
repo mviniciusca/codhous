ba1cfd726316df930fb6ffecaf1c5a2b48786e2e
Marcos Coelho <mviniciusca@gmail.com>
2024-09-13 11:08:04 -0300

    wip

--- app/Filament/Widgets/MaintenanceWidget.php
+++ app/Filament/Widgets/MaintenanceWidget.php
@@ -6,6 +6,7 @@ use Filament\Tables;
 use App\Models\Setting;
 use Filament\Forms\Get;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
 use Filament\Tables\Columns\ToggleColumn;
 use Filament\Widgets\TableWidget as BaseWidget;
 
@@ -23,6 +24,12 @@ class MaintenanceWidget extends BaseWidget
             ->description(__('Manage your app visibility here.'))
             ->paginated(false)
             ->striped()
+            ->headerActions([
+                Action::make('edit')
+                    ->label(__('Settings'))
+                    ->color('primary')
+                    ->url(route('filament.admin.resources.settings.edit', 'app'))
+            ])
             ->columns([
                 ToggleColumn::make('maintenance_mode')
                     ->label(__('Maintenance Mode')),
