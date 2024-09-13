9aeb8e3663ffe3fcd782c9ff511a375890619fbc
Marcos Coelho <mviniciusca@gmail.com>
2024-09-13 11:21:05 -0300

    wip

--- app/Filament/Widgets/BudgetWidget.php
+++ app/Filament/Widgets/BudgetWidget.php
@@ -2,10 +2,11 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\Budget;
 use Filament\Tables;
-use Filament\Tables\Columns\TextColumn;
+use App\Models\Budget;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Widgets\TableWidget as BaseWidget;
 
 class BudgetWidget extends BaseWidget
@@ -15,6 +16,12 @@ class BudgetWidget extends BaseWidget
     {
         return $table
             ->description(__('Quick view in your pending budgets here.'))
+            ->headerActions([
+                Action::make('edit')
+                    ->label(__('Budgets'))
+                    ->icon('heroicon-o-currency-dollar')
+                    ->url(route('filament.admin.resources.budgets.index'))
+            ])
             ->heading(__(
                 'Budget (' .
                 Budget::where('status', '=', 'pending')
