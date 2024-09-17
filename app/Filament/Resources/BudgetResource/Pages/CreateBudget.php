56f830d0be412cf0c76084afc11ac707aceddd10
Marcos Coelho <mviniciusca@gmail.com>
2024-09-17 15:23:25 -0300

    wip

--- app/Filament/Resources/BudgetResource/Pages/CreateBudget.php
+++ app/Filament/Resources/BudgetResource/Pages/CreateBudget.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\BudgetResource\Pages;
 
 use Filament\Actions;
+use App\Models\Setting;
 use Filament\Forms\Form;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\TextInput;
 use App\Filament\Resources\BudgetResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Forms\Components\DateTimePicker;
 
 class CreateBudget extends CreateRecord
 {
@@ -15,7 +23,130 @@ class CreateBudget extends CreateRecord
     {
         return $form
             ->schema([
-
+                Group::make()
+                    ->columnSpanFull()
+                    ->schema([
+                        Section::make(__('Budget Overview'))
+                            ->columns(4)
+                            ->description(__('Organize your budget report'))
+                            ->icon('heroicon-o-document')
+                            ->schema([
+                                Toggle::make('is_active')
+                                    ->helperText(__('Enable or disable this budget from the dashboard view'))
+                                    ->label(__('Active'))
+                                    ->inline(),
+                                Select::make('status')
+                                    ->helperText(__('Set the budget status'))
+                                    ->options([
+                                        'pending' => __('Pending'),
+                                        'on going' => __('On Going'),
+                                        'done' => __('Done'),
+                                        'ignored' => __('Ignored'),
+                                    ]),
+                                TextInput::make('code')
+                                    ->label(__('Budget Code'))
+                                    ->helperText(__('Use this code to search'))
+                                    ->disabled(),
+                                DateTimePicker::make('created_at')
+                                    ->format('d/m/Y H:i')
+                                    ->label(__('Date'))
+                                    ->disabled()
+                                    ->helperText(__('When this budget was created'))
+                            ]),
+                    ]),
+                Section::make('Budget Content')
+                    ->description(__('Here is the content from your budget'))
+                    ->icon('heroicon-o-shopping-bag')
+                    ->schema([
+                        Fieldset::make(__('Customer Information'))
+                            ->columns(3)
+                            ->schema([
+                                Group::make()
+                                    ->columns(3)
+                                    ->columnSpanFull()
+                                    ->schema([
+                                        TextInput::make('content.customer_name')
+                                            ->disabled()
+                                            ->dehydrated()
+                                            ->label(__('Customer Name')),
+                                        TextInput::make('content.customer_email')
+                                            ->disabled()
+                                            ->dehydrated()
+                                            ->label(__('Email')),
+                                        TextInput::make('content.customer_phone')
+                                            ->disabled()
+                                            ->dehydrated()
+                                            ->label(__('Phone')),
+                                    ]),
+                                TextInput::make('content.postcode')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('CEP')),
+                                TextInput::make('content.street')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('Street')),
+                                TextInput::make('content.number')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('Number')),
+                                TextInput::make('content.city')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('City')),
+                                TextInput::make('content.neighborhood')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('Neighborhood')),
+                                TextInput::make('content.state')
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->label(__('State')),
+                            ]),
+                        Fieldset::make('Construction Components')
+                            ->columns(4)
+                            ->schema([
+                                TextInput::make('content.quantity')
+                                    ->label(__('Quantity m³'))
+                                    ->suffix(__('m³'))
+                                    ->helperText(__('Min value is 3 (ABNT NBR 7212)'))
+                                    ->disabled()
+                                    ->dehydrated(),
+                                Select::make('content.object')
+                                    ->label(__('Local / Area'))
+                                    ->helperText(__('Local or area to be concreted'))
+                                    ->options(
+                                        Setting::query()
+                                            ->select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.area')
+                                    )
+                                    ->disabled()
+                                    ->dehydrated(),
+                                Select::make('content.fck')
+                                    ->label(__('FCK'))
+                                    ->helperText(__('Feature Compression Know'))
+                                    ->options(
+                                        Setting::query()
+                                            ->select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.fck')
+                                    )
+                                    ->disabled()
+                                    ->dehydrated(),
+                                Select::make('content.type')
+                                    ->label(__('Type'))
+                                    ->helperText(__('Type of Concrete'))
+                                    ->options(
+                                        Setting::query()
+                                            ->select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.type')
+                                    )
+                                    ->disabled()
+                                    ->dehydrated()
+                            ]),
+                    ]),
             ]);
     }
 }
--- app/Filament/Widgets/BudgetWidget.php
+++ app/Filament/Widgets/BudgetWidget.php
@@ -2,12 +2,16 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\User;
 use Filament\Tables;
 use App\Models\Budget;
 use App\Models\Setting;
 use Filament\Tables\Table;
 use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
 use Filament\Tables\Actions\CreateAction;
 use Filament\Tables\Columns\SelectColumn;
 use Filament\Widgets\TableWidget as BaseWidget;
@@ -24,10 +28,66 @@ class BudgetWidget extends BaseWidget
             )
             ->description(__('Quick view in your budgets here.'))
             ->headerActions([
-                CreateAction::make('new')
+                Action::make('new')
                     ->label(__('New'))
                     ->color('success')
-                    ->icon('heroicon-o-currency-dollar'),
+                    ->icon('heroicon-o-currency-dollar')
+                    ->modalHeading(__('Quick Simulation'))
+                    ->form([
+                        Section::make(__('Quantity & Pricing'))
+                            ->description(__('Quantity & Pricing'))
+                            ->label(__('Quantity & Pricing'))
+                            ->icon('heroicon-o-currency-dollar')
+                            ->columns(4)
+                            ->schema([
+                                Select::make('content.fck')
+                                    ->label(__('FCK'))
+                                    ->required()
+                                    ->options(
+                                        Setting::select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.fck', 'id')
+                                    ),
+                                Select::make('content.area')
+                                    ->label(__('Area'))
+                                    ->required()
+                                    ->options(
+                                        Setting::select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.area', 'id')
+                                    ),
+                                Select::make('content.type')
+                                    ->required()
+                                    ->label(__('Product'))
+                                    ->options(
+                                        Setting::select(['budget'])
+                                            ->get()
+                                            ->pluck('budget.type', 'id')
+                                    ),
+                                TextInput::make('content.quantity')
+                                    ->required()
+                                    ->label(__('Quantity'))
+                                    ->suffix('m³')
+                                    ->placeholder(__('quantity (m³)')),
+                            ]),
+                        Section::make(__('Locale & Shipment'))
+                            ->description(__('Quantity & Pricing'))
+                            ->label(__('Quantity & Pricing'))
+                            ->icon('heroicon-o-currency-dollar')
+                            ->schema([]),
+                        Section::make(__('Customer Information'))
+                            ->description(__('Quantity & Pricing'))
+                            ->label(__('Quantity & Pricing'))
+                            ->icon('heroicon-o-currency-dollar')
+                            ->columns(3)
+                            ->schema([
+                                TextInput::make('content.customer_name')
+                                    ->label(__('Name'))
+                                    ->placeholder(__('Admin Name'))
+                                    ->helperText(__('Admin Name'))
+                                    ->default(__(User::first()->name)),
+                            ]),
+                    ]),
                 Action::make('edit')
                     ->label(__('Budgets'))
                     ->label(__('View All'))
