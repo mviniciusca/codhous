7ad4a48b0824d28fb85a507a6b08da653ae185c6
Marcos Coelho <mviniciusca@gmail.com>
2024-09-17 11:42:09 -0300

    wip

--- app/Filament/Resources/BudgetResource/Pages/CreateBudget.php
+++ app/Filament/Resources/BudgetResource/Pages/CreateBudget.php
@@ -2,11 +2,20 @@
 
 namespace App\Filament\Resources\BudgetResource\Pages;
 
-use App\Filament\Resources\BudgetResource;
 use Filament\Actions;
+use Filament\Forms\Form;
+use App\Filament\Resources\BudgetResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBudget extends CreateRecord
 {
     protected static string $resource = BudgetResource::class;
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+            ]);
+    }
 }
