8668fa3d82cf65cf8ee357a5fe139438b0719390
Marcos Coelho <mviniciusca@gmail.com>
2024-10-18 14:03:46 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditCompany.php
+++ app/Filament/Resources/SettingResource/Pages/EditCompany.php
@@ -2,11 +2,30 @@
 
 namespace App\Filament\Resources\SettingResource\Pages;
 
-use App\Filament\Resources\SettingResource;
 use Filament\Actions;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use App\Filament\Resources\SettingResource;
 
 class EditCompany extends EditRecord
 {
     protected static string $resource = SettingResource::class;
+
+    public function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    {
+        return $form
+            ->schema([
+                Section::make(__('Company Settings'))
+                    ->icon('heroicon-o-building-office')
+                    ->description(__('Manager your Company Information. This information it will be used in Budget or Invoice Documents.'))
+                    ->relationship('setting')
+                    ->columns(3)
+                    ->schema([
+                        TextInput::make('name'),
+                        TextInput::make('email'),
+                        TextInput::make('phone'),
+                    ]),
+            ]);
+    }
 }
--- app/Models/CompanySetting.php
+++ app/Models/CompanySetting.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Setting;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CompanySetting extends Model
 {
