d46bd89688a70e5f8205335e54ff779e4eeb7f16
Marcos Coelho <mviniciusca@gmail.com>
2024-10-18 17:51:35 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditCompany.php
+++ app/Filament/Resources/SettingResource/Pages/EditCompany.php
@@ -33,34 +33,34 @@ class EditCompany extends EditRecord
                     ->columns(2)
                     ->schema([
                         TextInput::make('trade_name')
-                            ->helperText(__(''))
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Company Name. Visible in Budget / Invoice Document'))
                             ->label(__('Company Name')),
                         TextInput::make('legal_name')
-                            ->helperText(__(''))
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Legal Name. Visible in Budget / Invoice Document'))
                             ->label(__('Company Legal Name / Trade Name')),
                         TextInput::make('phone')
-                            ->helperText(__(''))
                             ->tel()
                             ->required()
                             ->maxLength(255)
                             ->prefix('+' . env('COUNTRY_CODE'))
                             ->mask('(99) 9999-9999')
+                            ->helperText(__('Visible in Budget / Invoice Document'))
                             ->label(__('Phone Number')),
                         TextInput::make('email')
-                            ->helperText(__(''))
                             ->email()
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Visible in Budget / Invoice Document'))
                             ->label(__('Email Address')),
                         TextInput::make('cnpj')
                             ->required()
                             ->maxLength(255)
                             ->mask('99.999.999/9999-99')
-                            ->helperText(__(''))
+                            ->helperText(__('Visible in Budget / Invoice Document'))
                             ->label(__('CNPJ'))
                             ->columnSpanFull(),
                     ]),
@@ -73,22 +73,27 @@ class EditCompany extends EditRecord
                         TextInput::make('address.postcode')
                             ->required()
                             ->maxLength(255)
-                            ->label(__('CEP'))
+                            ->label(__('Postcode'))
+                            ->helperText(__('Company Postcode'))
                             ->mask('99.999-999'),
                         TextInput::make('address.street')
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Company Street'))
                             ->label(__('Street')),
                         TextInput::make('address.number')
                             ->maxLength(255)
+                            ->helperText(__('Company Street Number (optional)'))
                             ->label(__('Number (optional)')),
                         TextInput::make('address.neighborhood')
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Company Neighborhood'))
                             ->label(__('Neighborhood')),
                         TextInput::make('address.city')
                             ->required()
                             ->maxLength(255)
+                            ->helperText(__('Company City'))
                             ->label(__('City')),
                         TextInput::make('address.state')
                             ->live()
@@ -98,6 +103,7 @@ class EditCompany extends EditRecord
                             ->afterStateUpdated(function (Set $set, ?string $state) {
                                 return $set('address.state', Str::upper($state));
                             })
+                            ->helperText(__('Company Federal State'))
                             ->label(__('UF')),
                     ]),
                 Section::make(__('Budget / Invoice Document Settings'))
