d70d15b853d557968a27e87795bbc69371bc5896
Marcos Coelho <mviniciusca@gmail.com>
2024-10-18 11:10:47 -0300

    wip

--- database/migrations/2024_10_15_200122_create_products_table.php
+++ database/migrations/2024_10_15_200122_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->string('name');
             $table->string('price');
             $table->boolean('is_active');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
