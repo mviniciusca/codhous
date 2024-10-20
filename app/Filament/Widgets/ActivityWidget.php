ee9e55991bc1648c8c3ded61d702c8663e17f887
Marcos Coelho <mviniciusca@gmail.com>
2024-10-20 15:31:28 -0300

    wip

--- app/Filament/Widgets/ActivityWidget.php
+++ app/Filament/Widgets/ActivityWidget.php
@@ -3,23 +3,32 @@
 namespace App\Filament\Widgets;
 
 use App\Models\ActivityLog;
-use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
-use Illuminate\Support\Facades\DB;
+use Filament\Tables\Actions\Action;
 
 class ActivityWidget extends BaseWidget
 {
     public function table(Table $table): Table
     {
         return $table
+            ->paginated(false)
+            ->heading(__('Access Recent Activity'))
+            ->headerActions([
+                Action::make('view_all')
+                    ->color('primary')
+                    ->label(__('View All'))
+                    ->icon('heroicon-o-arrow-up-right')
+                    ->url(route('filament.admin.resources.activity-logs.index'))
+            ])
             ->query(
                 ActivityLog::query()
                     ->where('event', '=', 'login')
                     ->take(5)
             )
             ->columns([
+                TextColumn::make('log_name')->badge(),
                 TextColumn::make('event'),
                 TextColumn::make('description'),
                 TextColumn::make('created_at')->dateTime('d/m/Y H:i'),
