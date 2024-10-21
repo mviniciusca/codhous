8f0902fcb5568b5dc90862aa09bd5b7a1fcc283e
Marcos Coelho <mviniciusca@gmail.com>
2024-10-20 23:46:19 -0300

    wip

--- app/Filament/Resources/MailResource/Pages/ViewMail.php
+++ app/Filament/Resources/MailResource/Pages/ViewMail.php
@@ -2,80 +2,86 @@
 
 namespace App\Filament\Resources\MailResource\Pages;
 
-use App\Filament\Resources\MailResource;
+use App\Models\Mail;
 use Filament\Actions;
-use Filament\Forms\Components\DateTimePicker;
-use Filament\Forms\Components\Section;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Toggle;
-use Filament\Resources\Pages\EditRecord;
+use Filament\Forms\Get;
+use Filament\Actions\Action;
+use Filament\Infolists\Infolist;
+use Illuminate\Support\HtmlString;
+use App\Filament\Resources\MailResource;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Infolists\Components\Section;
 use Illuminate\Contracts\Support\Htmlable;
+use Filament\Infolists\Components\TextEntry;
 
-class ViewMail extends EditRecord
+class ViewMail extends ViewRecord
 {
     protected static string $resource = MailResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('mark_favorite')
+                ->icon('heroicon-o-star')
+                ->color(function () {
+                    return $this->toggle('is_favorite');
+                })
+                ->action(function () {
+                    return '';
+                })
+                ->label(__('Fav')),
+            Action::make('mark_spam')
+                ->icon('heroicon-o-speaker-x-mark')
+                ->color('secondary')
+                ->label(__('Spam')),
             Actions\DeleteAction::make()
-                ->label(__('Trash'))
+                ->label('Move')
                 ->icon('heroicon-o-trash'),
         ];
+    }
+
+    public function toggle(?string $column)
+    {
+
+        $check = Mail::select($column)
+            ->where('id', '=', request()->route('record'))
+            ->first([$column]);
+
+        if ($check) {
+            if ($check[$column] == true) {
+                return 'primary';
+            } else {
+                return 'secondary';
+            }
+        }
     }
     public function getTitle(): Htmlable|string
     {
         return __('Mail');
     }
 
-    public function form(\Filament\Forms\Form $form): \Filament\Forms\Form
+    public function infolist(Infolist $infolist): Infolist
     {
-        return $form
+        return $infolist
             ->schema([
-                Section::make(__('Actions'))
-                    ->description(__('Actions for this message'))
-                    ->icon('heroicon-o-cog-6-tooth')
-                    ->collapsible()
-                    ->columns(3)
-                    ->schema([
-                        Toggle::make('is_favorite')
-                            ->inline(true)
-                            ->label(__('Mark as Important')),
-                        Toggle::make('is_read')
-                            ->inline(true)
-                            ->label(__('Mark as Read')),
-                        Toggle::make('is_spam')
-                            ->inline(true)
-                            ->label(__('Tag as Spam')),
-                    ]),
-                Section::make(__('Mail Message'))
-                    ->description(__('Your mail content'))
+                Section::make(__('Message'))
                     ->icon('heroicon-o-envelope')
-                    ->collapsible()
                     ->columns(3)
                     ->schema([
-                        TextInput::make('name')
-                            ->label(__('From:'))
-                            ->disabled(),
-                        TextInput::make('email')
-                            ->label(__('Email:'))
-                            ->disabled(),
-                        DateTimePicker::make('created_at')
-                            ->label(__('Date / Hour: '))
-                            ->format('d/m/y H:i:s')
-                            ->disabled(),
-                        TextInput::make('subject')
-                            ->label(__('Subject:'))
+                        TextEntry::make('name')
+                            ->label(new HtmlString(__('<strong>From:</strong>'))),
+                        TextEntry::make('email')
+                            ->label(new HtmlString(__('<strong>Email:</strong>'))),
+                        TextEntry::make('created_at')
+                            ->dateTime('d/m/y H:i')
+                            ->label(new HtmlString(__('<strong>Received At</strong>'))),
+                        TextEntry::make('subject')
                             ->columnSpanFull()
-                            ->disabled(),
-                        Textarea::make('message')
-                            ->label(__('Message:'))
+                            ->label(new HtmlString(__('<strong>Subject:</strong>'))),
+                        TextEntry::make('message')
                             ->columnSpanFull()
-                            ->rows(5)
-                            ->disabled(),
-                    ]),
+                            ->label(new HtmlString(__('<strong>Message:</strong>'))),
+                    ])
             ]);
     }
 
