5df67c606cec76a525398ea430c2b16136560f25
Marcos Coelho <mviniciusca@gmail.com>
2024-10-22 16:35:32 -0300

    wip

--- app/Filament/Resources/BudgetResource/Pages/EditBudget.php
+++ app/Filament/Resources/BudgetResource/Pages/EditBudget.php
@@ -384,17 +384,6 @@ class EditBudget extends EditRecord
         }
     }
 
-    // private function updateBudgetStatus(Get $get, Set $set, ?string $state): string
-    // {
-    //     $tax = $get('content.tax');
-    //     $discount = $get('content.discount');
-    //     $currentStatus = $get('status');
-    //     if ($currentStatus === 'pending' && ($tax != 0 or $tax != null) && ($discount != 0 or $discount != null)) {
-    //         return $set('status', 'on going');
-    //     } else {
-    //         return $set('status', $currentStatus);
-    //     }
-    // }
     protected function getHeaderActions(): array
     {
         return [
