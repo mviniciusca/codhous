08092a2d5cfccd9cd04be2ffa6682e69a6080fd7
Marcos Coelho <mviniciusca@gmail.com>
2024-10-24 15:21:52 -0300

    wip

--- app/Filament/Resources/ProductOptionResource/Pages/ListProductOptions.php
+++ app/Filament/Resources/ProductOptionResource/Pages/ListProductOptions.php
@@ -13,7 +13,9 @@ class ListProductOptions extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->icon('heroicon-o-plus')
+                ->label(__('New Option')),
         ];
     }
 }
