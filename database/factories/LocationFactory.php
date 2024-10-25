12dd4718d5899e3adc3e36b949880a3a2a644db0
Marcos Coelho <mviniciusca@gmail.com>
2024-10-25 15:28:14 -0300

    wip

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -17,7 +17,7 @@ class LocationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->country()
         ];
     }
 }
--- database/factories/Product/OptionFactory.php
+++ database/factories/Product/OptionFactory.php
@@ -17,7 +17,7 @@ class OptionFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->city()
         ];
     }
 }
