88e50a24f954975d11df9b1b12a75c228e4ba0ca
Marcos Coelho <mviniciusca@gmail.com>
2024-10-25 23:17:59 -0300

    wip

--- app/Filament/Resources/ProductOptionResource/RelationManagers/ProductRelationManager.php
+++ app/Filament/Resources/ProductOptionResource/RelationManagers/ProductRelationManager.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Resources\ProductOptionResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Tables\Actions\ActionGroup;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class ProductRelationManager extends RelationManager
 {
@@ -27,19 +28,26 @@ class ProductRelationManager extends RelationManager
     public function table(Table $table): Table
     {
         return $table
+            ->heading(__('Products in this option'))
+            ->description(__('Manager from here products under this option'))
             ->recordTitleAttribute('name')
             ->columns([
                 Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('price'),
             ])
             ->filters([
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                Tables\Actions\CreateAction::make()
+                    ->icon('heroicon-o-shopping-bag')
+                    ->label(__('New Product')),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                ActionGroup::make([
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make()
+                ]),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
