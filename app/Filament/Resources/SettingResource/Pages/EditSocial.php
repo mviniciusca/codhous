35dce77b4478280487b0817489cbafed0fbb696a
Marcos Coelho <mviniciusca@gmail.com>
2024-10-27 11:18:01 -0300

    wip

--- app/Filament/Resources/SettingResource/Pages/EditSocial.php
+++ app/Filament/Resources/SettingResource/Pages/EditSocial.php
@@ -2,26 +2,29 @@
 
 namespace App\Filament\Resources\SettingResource\Pages;
 
+use App\Filament\Resources\SettingResource;
 use Filament\Actions;
-use Filament\Forms\Form;
-use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
 use Filament\Forms\Set;
 use Filament\Resources\Pages\EditRecord;
-use App\Filament\Resources\SettingResource;
 use Illuminate\Contracts\Support\Htmlable;
 use Illuminate\Support\Str;
 
 class EditSocial extends EditRecord
 {
     protected static string $resource = SettingResource::class;
+
     protected static ?string $navigationIcon = 'heroicon-o-share';
+
     public static function getNavigationLabel(): string
     {
         return __('Social Network');
     }
+
     public function getTitle(): string|Htmlable
     {
         return __('Social Network Settings');
@@ -48,7 +51,7 @@ class EditSocial extends EditRecord
                                     ->columnSpan(2)
                                     ->live()
                                     ->debounce(700)
-                                    ->afterStateUpdated(fn(Set $set, ?string $state) => $set('icon', Str::slug($state)))
+                                    ->afterStateUpdated(fn (Set $set, ?string $state) => $set('icon', Str::slug($state)))
                                     ->helperText(__('ex: Facebook'))
                                     ->label(__('Social Network')),
                                 TextInput::make('link')
