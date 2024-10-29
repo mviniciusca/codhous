18a0e5d844d50f73e88fc6042676a8572822c5ae
Marcos Coelho <mviniciusca@gmail.com>
2024-10-29 14:24:48 -0300

    wip

--- app/Filament/Resources/NewsletterResource/Pages/ListNewsletters.php
+++ app/Filament/Resources/NewsletterResource/Pages/ListNewsletters.php
@@ -2,29 +2,37 @@
 
 namespace App\Filament\Resources\NewsletterResource\Pages;
 
-use Filament\Actions;
-use Filament\Resources\Pages\ListRecords;
-use Illuminate\Contracts\Support\Htmlable;
 use App\Filament\Resources\NewsletterResource;
 use App\Filament\Resources\NewsletterResource\Widgets\NewsletterOverwview;
+use Filament\Actions;
+use Filament\Actions\Action;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
 
 class ListNewsletters extends ListRecords
 {
     protected static string $resource = NewsletterResource::class;
+
     public function getTitle(): string|Htmlable
     {
         return __('Subscribers');
     }
+
     protected function getHeaderWidgets(): array
     {
         return [
             NewsletterOverwview::class,
         ];
     }
+
     protected function getHeaderActions(): array
     {
         return [
-            //
+            Action::make('view_bin')
+                ->label(false)
+                ->icon('heroicon-o-trash')
+                ->url(route('filament.admin.resources.subscribers.bin'))
+                ->color('gray'),
         ];
     }
 }
