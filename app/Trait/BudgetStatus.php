3d5bc621a20f93144b34a9d30e4b46ccdc7fc3fa
Marcos Coelho <mviniciusca@gmail.com>
2024-10-29 13:16:09 -0300

    wip

--- app/Trait/BudgetStatus.php
+++ app/Trait/BudgetStatus.php
@@ -12,7 +12,9 @@ trait BudgetStatus
         $tax = $get('content.tax');
         $discount = $get('content.discount');
         $currentStatus = $get('status');
-        if ($currentStatus === 'pending' && ($tax != 0 or $tax != null) && ($discount != 0 or $discount != null)) {
+
+        if ($currentStatus === 'pending' && ($tax != 0 or $tax != null)
+        && ($discount != 0 or $discount != null)) {
             return $set('status', 'on going');
         } else {
             return $set('status', $currentStatus);
