d766d1cefd8fe049751dc0a67cbe21f69240aab9
Marcos Coelho <mviniciusca@gmail.com>
2024-11-01 20:18:34 -0300

    wip

--- resources/views/budget/history-view.blade.php
+++ resources/views/budget/history-view.blade.php
@@ -6,12 +6,12 @@
             class="relative fi-fo-tabs flex flex-col fi-contained rounded-xl bg-white shadow-sm ring-1 ring-gray-950/5 dark:bg-gray-900 dark:ring-white/10">
             <div class="rounded-lg p-4">
                 <div class="flex items-center justify-between">
-                    <h2 class="text-base font-semibold flex flex-initial gap-2 leading-6 text-gray-950 dark:text-white">
+                    <h2 class="flex items-center gap-2 text-sm font-medium leading-6 text-gray-950 dark:text-white">
                         <span>
 
                             @if($budget->action === 'update')
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-6 w-6 mr-4">
+                            <svg xmlns=" http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="h-6 w-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
                             </svg>
@@ -20,7 +20,7 @@
 
                             @if($budget->action === 'create')
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-6 w-6 mr-4">
+                                stroke="currentColor" class="h-6 w-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12" />
                             </svg>
@@ -29,25 +29,27 @@
 
                             @if($budget->action === 'delete')
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="h-6 w-6 mr-4">
+                                stroke="currentColor" class="h-6 w-6">
                                 <path stroke-linecap=" round" stroke-linejoin="round"
                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
                             @endif
 
-
-
                         </span>
-                        {{ $budget->user->name }}
+                        <span class="mt-2 fi-section-header-description overflow-hidden break-words text-sm text-gray-500
+                        dark:text-gray-400">{{ $budget->user->name }}
+                            {{ $budget->action === 'update' ? 'updated' : 'created'
+                            }} {{ ' this budget. '}}
+                        </span>
                     </h2>
+
+
                     <span class="fi-fo-field-wrp-helper-text break-words text-sm text-gray-500">
                         {{ $budget->updated_at->format('d/m/Y H:i') }}
                     </span>
+
                 </div>
-                <p
-                    class="mt-2 fi-section-header-description overflow-hidden break-words text-sm text-gray-500 dark:text-gray-400">
-                    {{ $budget->user->name }} has {{ $budget->action === 'update' ? 'updated' : 'created'
-                    }} {{ ' this budget on ' . $budget->created_at->format('d/m/Y H:i') }}</p>
+
             </div>
         </div>
 
