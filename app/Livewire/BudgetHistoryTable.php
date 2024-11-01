83291d1ffd7f59c3c3ce9d5bbdae0f98f62b1f4e
Marcos Coelho <mviniciusca@gmail.com>
2024-10-31 23:16:01 -0300

    wip

--- app/Livewire/BudgetHistoryTable.php
+++ app/Livewire/BudgetHistoryTable.php
@@ -6,12 +6,31 @@ use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
 use Livewire\Component;
 
 class BudgetHistoryTable extends Component implements HasForms, HasTable
 {
     use InteractsWithTable;
     use InteractsWithForms;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(Budget::query())
+            ->columns([
+                TextColumn::make('name'),
+            ])
+            ->filters([
+                // ...
+            ])
+            ->actions([
+                // ...
+            ])
+            ->bulkActions([
+                // ...
+            ]);
+    }
 
     public function render()
     {
