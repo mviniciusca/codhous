a7eb153ff01f8acfef8dde73da65916a4d213f52
Marcos Coelho <mviniciusca@gmail.com>
2024-11-02 22:38:43 -0300

    wip

---
+++ database/migrations/2024_11_03_011551_create_user_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->boolean('is_active')->default(true);
+            $table->enum('role', ['admin', 'team']);
+            $table->string('name');
+            $table->string('email');
+            $table->string('picture')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_settings');
+    }
+};
