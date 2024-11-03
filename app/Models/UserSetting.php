0c0796f1ead01cf06c7a46602f652f4f09a0944d
Marcos Coelho <mviniciusca@gmail.com>
2024-11-02 22:43:26 -0300

    wip

--- app/Models/UserSetting.php
+++ app/Models/UserSetting.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,11 @@ class UserSetting extends Model
 {
     /** @use HasFactory<\Database\Factories\UserSettingFactory> */
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
