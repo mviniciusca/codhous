5ed99f223021660edfdc4f3b46945210f19d63d0
Marcos Coelho <mviniciusca@gmail.com>
2025-03-05 19:47:14 -0300

    updated dashboard icon badge from stats widget

--- app/Filament/Widgets/StatsWidget.php
+++ app/Filament/Widgets/StatsWidget.php
@@ -23,7 +23,7 @@ class StatsWidget extends BaseWidget
             Stat::make(__('Application Status'), '')
                 ->label(__('Website Status'))
                 ->icon('heroicon-o-wifi')
-                ->url(route('filament.admin.resources.settings.edit_maintenance', Setting::first()))
+                ->url(route('filament.admin.resources.settings.edit_maintenance', Setting::first()->value('id')))
                 ->value(view('components.badge', [
                     'status' => Setting::select(['discovery_mode', 'maintenance_mode'])
                         ->first(),
@@ -31,17 +31,19 @@ class StatsWidget extends BaseWidget
                 ->description(Setting::first()
                     ->maintenance_mode ?
                     __('Maintenance Mode is Active') : __('Application is Live'))
-                ->color('secondary'),
+                ->color('primary'),
 
             Stat::make('Mailing List', Newsletter::withoutTrashed()->count())
                 ->icon('heroicon-o-envelope')
                 ->chart($this->chartData(Newsletter::class)->toArray())
+                ->color('primary')
                 ->description(__('Your Subscribers'))
                 ->url(route('filament.admin.resources.subscribers.index'))
                 ->descriptionIcon('heroicon-m-inbox-stack'),
 
             Stat::make(__('Budgets'), Budget::withoutTrashed()->count())
                 ->icon('heroicon-o-currency-dollar')
+                ->color('primary')
                 ->chart($this->chartData(Budget::class)->toArray())
                 ->description(__('Total of Budgets'))
                 ->url(route('filament.admin.resources.budgets.index'))
@@ -49,6 +51,7 @@ class StatsWidget extends BaseWidget
 
             Stat::make(__('Customers'), Customer::withoutTrashed()->count())
                 ->icon('heroicon-o-user')
+                ->color('primary')
                 ->chart($this->chartData(Customer::class)->toArray())
                 ->url(route('filament.admin.resources.customers.index'))
                 ->description(__('Total of Customers'))
