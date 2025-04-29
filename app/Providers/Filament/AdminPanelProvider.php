41a059354fde354cf35e6e182884597df9000d3a
Marcos Coelho <mviniciusca@gmail.com>
2025-04-29 18:39:29 -0300

    remove unused theme and tailwind configuration files from the admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,6 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
-            ->viteTheme('resources/css/filament/admin/theme.css')
             ->breadcrumbs(false)
             ->id('admin')
             ->path('admin')
