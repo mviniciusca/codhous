78318acb66edc8e45c83e33879ecaf063f8383c6
Marcos Coelho <mviniciusca@gmail.com>
2026-05-07 09:12:49 -0300

    feat: override default dashboard page to customize title and navigation label

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Dashboard as BaseDashboard;
+
+class Dashboard extends BaseDashboard
+{
+    protected static ?string $title = '';
+
+    protected static ?string $navigationLabel = 'Dashboard';
+}
